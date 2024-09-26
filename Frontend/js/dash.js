ea6391c849d05ca4dbc6ada200ac7d0675afe679
Matheuskocotem <kocotemmatheus@gmail.com>
2024-09-26 09:17:36 -0300

    img dash cart começo

---
+++ Frontend/js/dash.js
@@ -0,0 +1,17 @@
+function toggleDropdown() {
+    const dropdown = document.getElementById('userDropdown');
+    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+}
+
+// Fecha o dropdown se clicar fora
+window.onclick = function(event) {
+    if (!event.target.matches('.fa-user')) {
+        const dropdowns = document.getElementsByClassName("dropdown");
+        for (let i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown.style.display === 'block') {
+                openDropdown.style.display = 'none';
+            }
+        }
+    }
+}
